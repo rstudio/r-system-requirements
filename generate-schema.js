55e5646bf3731891c54261ca5f26f4eb9ababca9
Jonathan Yoder <jonyoder@gmail.com>
2019-03-29 12:06:22 -0400

    Schema Generator and template

---
+++ generate-schema.js
@@ -0,0 +1,94 @@
+let fs = require('fs')
+
+const distros = [
+    {
+        "name": "debian",
+        "distros": [ "debian" ],
+        "versions": [ "stretch", "buster" ]
+    },
+    {
+        "name": "ubuntu",
+        "distros": [ "ubuntu" ],
+        "versions": [ "xenial", "bionic" ]
+    },
+    {
+        "name": "centos",
+        "distros": [ "centos", "redhat" ],
+        "versions": [ "6", "7" ]
+    },
+    {
+        "name": "opensuse",
+        "distros": [ "opensuse" ],
+        "versions": [ "42", "15" ]
+    },
+    {
+        "name": "sle",
+        "distros": [ "sle" ],
+        "versions": [ "12", "15" ]
+    }
+]
+
+const distro_template = function(os, name, distros) {
+    return {
+        "properties": {
+            "os": {"const": os},
+            "distribution": {
+                "enum": distros,
+            },
+            "version": {
+                "type": "object",
+                "properties": {
+                    "exactly": {
+                        "$ref": `#/definitions/versions/${name}`
+                    },
+                    "range": {
+                        "type": "object",
+                        "properties": {
+                            "at_least": {
+                                "$ref": `#/definitions/versions/${name}`
+                            },
+                            "at_most": {
+                                "$ref": `#/definitions/versions/${name}`
+                            }
+                        },
+                        "required": ["at_least", "at_most"],
+                        "additionalProperties": false
+                    }
+                },
+                "oneOf": [
+                    {
+                        "required": ["exactly"]
+                    },
+                    {
+                        "required": ["range"]
+                    }
+                ],
+                "additionalProperties": false
+            }
+        },
+        "required": ["os"],
+        "additionalProperties": false
+    }
+}
+
+const template = fs.readFileSync('schema.template.json')
+const schema = JSON.parse(template)
+const defs = {
+    versions: {}
+}
+
+for (let i=0; i < distros.length; i++) {
+    const distro = distros[i]
+    let distroEnum = []
+    for (let i=0; i < distro.versions.length; i++) {
+        const ver = distro.versions[i]
+        distroEnum = distroEnum.concat(ver)
+    }
+    defs.versions[distro.name] = {
+        enum: distroEnum
+    }
+    defs[distro.name] = distro_template('linux', distro.name, distro.distros)
+}
+
+schema.definitions = defs
+fs.writeFileSync("schema.json", JSON.stringify(schema, null, 2))
