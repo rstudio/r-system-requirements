5d76d4d9dfdb69f1fd4be128ab45712edf184b4e
Greg Lin <glin@rstudio.com>
2021-03-05 16:15:30 -0600

    Add package version to sysreqs test data
    More info to tell apart packages with multiple versions in CRAN, like XML.

--- test/get-sysreqs.R
+++ test/get-sysreqs.R
@@ -1,10 +1,11 @@
 # Gets a JSON list of CRAN packages with SystemRequirements
 
 db <- tools::CRAN_package_db()
-sysreqs <- db[!is.na(db$SystemRequirements), c("Package", "SystemRequirements")]
+sysreqs <- db[!is.na(db$SystemRequirements), c("Package", "Version", "SystemRequirements")]
 
 sysreqs_json <- sapply(seq_len(nrow(sysreqs)), function(i) {
   package <- sysreqs[i, "Package"]
+  version <- sysreqs[i, "Version"]
   reqs <- sysreqs[i, "SystemRequirements"]
 
   # Remove line breaks and escape double-quotes
@@ -13,8 +14,9 @@ sysreqs_json <- sapply(seq_len(nrow(sysreqs)), function(i) {
 
   sprintf('  {
     "Package": "%s",
+    "Version": "%s",
     "SystemRequirements": "%s"
-  }', package, reqs)
+  }', package, version, reqs)
 })
 
 sysreqs_json <- c("[", paste(sysreqs_json, collapse = ",\n"), "]")
